e0e788b931f609537813635403b814a9add04c2c
juSt-jeLLy <yagneshpatel931@gmail.com>
2025-02-08 23:04:49 +0530

    Build Fix

--- client/app/context/index.tsx
+++ client/app/context/index.tsx
@@ -1,28 +1,69 @@
-"use client"
+// context/index.tsx
+"use client";
 
-import { config } from "../config"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import React, { type ReactNode } from "react"
-import { cookieToInitialState, WagmiProvider } from 'wagmi'
+import { wagmiAdapter, projectId } from "../config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAppKit } from "@reown/appkit/react";
+import {
+  mainnet,
+  arbitrum,
+  avalanche,
+  base,
+  optimism,
+  polygon,
+  sepolia,
+} from "@reown/appkit/networks";
+import React, { type ReactNode } from "react";
+import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
 
-const queryClient = new QueryClient()
+// Set up queryClient
+const queryClient = new QueryClient();
 
+if (!projectId) {
+  throw new Error("Project ID is not defined");
+}
+
+// Set up metadata
+const metadata = {
+  name: "clashofclout",
+  description: "AppKit Example",
+  url: "https://firmly-pet-goat.ngrok-free.app", // origin must match your domain & subdomain
+  icons: ["https://assets.reown.com/reown-profile-pic.png"],
+};
+
+// Create the modal
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks: [mainnet, arbitrum, avalanche, base, optimism, polygon, sepolia],
+  defaultNetwork: mainnet,
+  metadata,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
+
+export { modal };
 function ContextProvider({
   children,
   cookies,
 }: {
-  children: ReactNode
-  cookies: string | null
+  children: ReactNode;
+  cookies: string | null;
 }) {
-  const initialState = cookieToInitialState(config, cookies)
+  const initialState = cookieToInitialState(
+    wagmiAdapter.wagmiConfig as Config,
+    cookies
+  );
 
   return (
-    <WagmiProvider config={config} initialState={initialState}>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
+    <WagmiProvider
+      config={wagmiAdapter.wagmiConfig as Config}
+      initialState={initialState}
+    >
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
-  )
+  );
 }
 
-export default ContextProvider
+export default ContextProvider;
